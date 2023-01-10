36a713d93b489f5b6faac5c2863cb40399add1e6
Shivam Kumar <sshivam962@gmail.com>
2023-01-10 14:44:05 +0530

    Added another DB migration

--- db/migrate/20230110070858_create_products.rb
+++ db/migrate/20230110070858_create_products.rb
@@ -1,8 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :products do |t|
+  def up
+    Product.connection.create_table :products do |t|
+      t.string :product_name
+      t.string :description
 
       t.timestamps
     end
+  end
+  
+  def down
+    Product.connection.drop_table :products
   end
 end
